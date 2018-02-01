daf0ffac55df22c77a3dde1099c7a11298cbda22
Jens John <dev@2ion.de>
2018-02-01 17:51:50 +0100

    Bash completion: add --help

--- bash_completion.sh
+++ bash_completion.sh
@@ -2,7 +2,7 @@
 _apt_sourcemgr () {
   local cur=${COMP_WORDS[COMP_CWORD]}
   local prev=${COMP_WORDS[COMP_CWORD-1]}
-  local dashed="-F --file  -D --disabled  -E --enabled  -I --invalid  -a --architecture  -c --component  -d --distribution  -f --fuzzy  -r --regex  -t --type  -u --uri  -s --simulate  -T --template"
+  local dashed="-F --file  -D --disabled  -E --enabled  -I --invalid  -a --architecture  -c --component  -d --distribution  -f --fuzzy -h --help -r --regex  -t --type  -u --uri  -s --simulate  -T --template"
   local verbs="add remove enable disable apply-template"
   if [[ -z "$cur"  ]]; then
     COMPREPLY=($(compgen -W "$dashed $verbs" -- "$cur"))
